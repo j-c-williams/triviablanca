3ed42d771d4f31ac7b7b5b7541096f2d652e5e34
Jackson Williams <jacksonwilliams.williams@gmail.com>
2024-09-03 17:09:06 -0600

    updates landing

--- triviablanca2/src/app/app.routes.ts
+++ triviablanca2/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LandingComponent } from './landing/landing.component';
+import { YubaComponent } from './yuba/yuba.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'yuba', component: YubaComponent },
+  // Add more routes as needed
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- triviablanca2/src/app/landing/landing.component.ts
+++ triviablanca2/src/app/landing/landing.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  standalone: true,
-  imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  startGame(): void {
+    this.router.navigate(['/yuba']);
+  }
 }
