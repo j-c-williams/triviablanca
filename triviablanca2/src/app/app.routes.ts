e169c19667de73e8e0422a29cc8795fd4532c893
Jackson Williams <jacksonwilliams.williams@gmail.com>
2024-09-04 13:07:52 -0600

    first question done

--- triviablanca2/src/app/app.routes.ts
+++ triviablanca2/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
 import { YubaComponent } from './yuba/yuba.component';
+import { JanComponent } from './jan/jan.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'yuba', component: YubaComponent },
+  { path: 'jan', component: JanComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' },
   // Add more routes as needed
 ];
---
+++ triviablanca2/src/app/jan/jan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jan',
+  standalone: true,
+  imports: [],
+  templateUrl: './jan.component.html',
+  styleUrl: './jan.component.css'
+})
+export class JanComponent {
+
+}
