b3403f3ef6f65a6304dfdde8d0946ddfde61fb8f
Jackson Williams <jacksonwilliams.williams@gmail.com>
2024-09-05 18:49:20 -0600

    sixteenth question done

---
+++ triviablanca2/src/app/anagram/anagram.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import * as CryptoJS from 'crypto-js';
+
+@Component({
+  selector: 'app-anagram',
+  standalone: true,
+  imports: [],
+  templateUrl: './anagram.component.html',
+  styleUrl: './anagram.component.css'
+})
+export class AnagramComponent {
+  correctAnswer: string = '550b8ce3d164393f314eb1ba5a8cc74face29b90f9c94242c7182c448aa6a4c1';
+  wrongAnswerText: string = ''
+  
+  @ViewChild('answerInput') answerInput!: ElementRef;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.answerInput.nativeElement.focus();
+    });
+  }
+
+  checkAnswer(inputValue: string) {
+    const sanitizedInput = inputValue.trim().toLowerCase().replace(/\s+/g, '');
+    const hashedInput = CryptoJS.SHA256(sanitizedInput).toString(CryptoJS.enc.Hex);
+    this.wrongAnswerText = ""
+        
+    if (hashedInput === this.correctAnswer || hashedInput === 'ab45e8474269b026f668d49860b36850122e18a50d5ea38f3fefdae08261865c') {
+      this.router.navigate(['../landing']);
+    } else {
+      this.wrongAnswerText = "Nope, try again"
+    }
+  }
+}
--- triviablanca2/src/app/tx/tx.component.ts
+++ triviablanca2/src/app/tx/tx.component.ts
@@ -29,7 +29,7 @@ export class TxComponent {
     this.wrongAnswerText = "";
     
     if (hashedInput === this.correctAnswer || hashedInput === '6c636ca367c932600d6b1debbb0a2f302120fdc72ca6619a886e004e05c90cf8') {
-      this.router.navigate(['../tx']);
+      this.router.navigate(['../16_14_15_5_6_3_13_4_7_9_10_11_12_19_17_18_8_2_1']);
     } else if (hashedInput === 'b938bde0827e61dc5a8e055bc81210762eb79d1b4fa80826d66f0ca8c22a4f73' || hashedInput == '86fa9ea28ce1daa73704a540456421ba83765e8647d206c0acfb77781464e8ea') {
       this.wrongAnswerText = "On the right track... Where is that?"
     } else if (hashedInput === '92a4abafbf5c677bfa0d17ecb43ccbaaa013c14c2046c71d94b56e2025b019b8' || hashedInput == 'f9fbe2b9ab9b9a04de4ff8f15384a8750594a521d1094af76987bf039b948192') {
